0ecb9f52347260abe199b06eba3a330624c0ed4a
Yegan <yeganj@gmail.com>
2018-09-07 17:36:40 +0200

    added eslint

---
+++ greetingfactoryfunc.js
@@ -0,0 +1,59 @@
+module.exports = function (pool) {
+  // the checkGreet async function adds name into the database
+  async function checkGreet (name, language) { 
+
+    if (name != '') {
+      let result = await pool.query('select * from greetings where name = $1', [name.toLowerCase(name)])
+      if (result.rowCount === 0) {
+        await pool.query('INSERT INTO greetings (name, counter) values ($1, $2)', [name.toLowerCase(name), 0])
+      }
+      await pool.query('UPDATE greetings SET counter = counter + 1 WHERE name = $1', [name.toLowerCase(name)])
+    }
+
+    if (language === 'English') {
+      return 'Hello ' + name
+    }
+    if (language === 'Spanish') {
+      return 'Hola ' + name
+    }
+    if (language === 'French') {
+      return 'Bonjour ' + name
+
+    }
+
+  }
+  // the names function gets all the names from the database
+
+  async function names () {
+    let names = await pool.query('select name from greetings')
+    return names.rows
+  }
+
+  // the count function gets the table from the database
+
+  async function count () {
+    let result = await pool.query('SELECT * FROM greetings')
+    return result.rowCount
+  }
+
+  // the reset function deletes all entries in the database
+  async function reset () {
+    let reset = await pool.query('delete from greetings')
+    return reset
+  }
+
+  // the oneName function gets the rows from the table
+
+  async function oneName (Myname) {
+    let results = await pool.query('select * from greetings where name =$1', [Myname])
+    return results.rows[0]
+  }
+
+  return {
+    checkGreet,
+    count,
+    names,
+    reset,
+    oneName
+  }
+}
--- index.js
+++ index.js
@@ -1,136 +1,124 @@
 const express = require('express')
 const app = express()
-const flash = require('express-flash');
-const session = require('express-session');
+const flash = require('express-flash')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
-const greetingFunc = require('./GreetingFactoryFunc')
-const postgres = require('pg');
-const Pool = postgres.Pool;
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/greetings';
+const greetingFunc = require('./greetingfactoryfunc')
+const postgres = require('pg')
+const Pool = postgres.Pool
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/greetings'
 const pool = new Pool({
-    connectionString
-});
+  connectionString
+})
 const funcGreeting = greetingFunc(pool)
 
-let useSSL = false;
-let local = process.env.LOCAL || false;
+let useSSL = false
+let local = process.env.LOCAL || false
 if (process.env.DATABASE_URL && !local) {
-    useSSL = true;
+  useSSL = true
 }
 
 // Handlebar engine allowing for templating of data
 
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
+  defaultLayout: 'main'
 
 }))
 
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }))
 
 // initialise session middleware - flash-express depends on it
 app.use(session({
-    secret: "<add a secret string here>",
-    resave: false,
-    saveUninitialized: true
-}));
+  secret: '<add a secret string here>',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // initialise the flash middleware
-app.use(flash());
+app.use(flash())
 app.use(bodyParser.json())
 
 // get route that displays the number upon the page being loaded
 
 app.get('/', async function (req, res, next) {
-    try {
-        let counterNum = await funcGreeting.count();
-        res.render('home', { counterNum })
-    }
+  try {
+    let counterNum = await funcGreeting.count()
+    res.render('home', { counterNum })
+}
 
-    catch (err) {
-        next(error.stack)
-    }
-
+  catch (error) {
+    next(error.stack)
+  }
 
 })
 
 // Post route that inputs from the user ie the name(Andrew) , and displaying this on the home.handlebars page
 
 app.post('/greetings', async function (req, res, next) {
-    try {
-        let nameEntered = req.body.inputText;
-        let languageSelected = req.body.language;
-        if (nameEntered === '' || nameEntered === undefined) {
-            req.flash('error', 'Please enter a name')
-        } else {
-            if (!languageSelected) {
-                req.flash('message', 'Please select a language');
-                return res.redirect('/');
-            }
-        }
-        
-        let displayName = await funcGreeting.checkGreet(nameEntered, languageSelected);
-        let counterNum = await funcGreeting.count();
-        res.render('home', { displayName: displayName, counterNum: counterNum, clear: counterNum })
-    } catch (error) {
-        next(error.stack);
+  try {
+    let nameEntered = req.body.inputText
+    let languageSelected = req.body.language
+    if (nameEntered === '' || nameEntered === undefined) {
+      req.flash('error', 'Please enter a name')
+    } else {
+      if (!languageSelected) {
+        req.flash('message', 'Please select a language')
+        return res.redirect('/')
+      }
     }
-
-
-});
-
-
-
-
-
+    let displayName = await funcGreeting.checkGreet(nameEntered, languageSelected)
+    let counterNum = await funcGreeting.count()
+    res.render('home', { displayName: displayName, counterNum: counterNum, clear: counterNum })
+  } catch (error) {
+    next(error.stack)
+  }
+})
 // this get route display the name greeted and the language selected to be greeted in
 
 app.get('/namesGreeted', async function (req, res, next) {
 
-    try {
-        let namesDisplay = await funcGreeting.names();
-        res.render('nameDisplayPage', { namesDisplay: namesDisplay });
-    }
+  try {
+    let namesDisplay = await funcGreeting.names();
+    res.render('nameDisplayPage', { namesDisplay: namesDisplay });
+  }
 
-    catch (error) {
-        next(error.stack)
-    }
+  catch (error) {
+    next(error.stack)
+  }
 })
 
 // this post route resets the counter and the names in the database by deleting the entries in the database
 
 app.post('/reset', async function (req, res, next) {
-    try {
-        let reset = await funcGreeting.reset();
-        res.redirect('/')
-    }
-    catch (error) {
-        next(error.stack)
-    }
+  try {
+    let reset = await funcGreeting.reset()
+    res.redirect('/')
+  }
+  catch (error) {
+    next(error.stack)
+  }
 })
 
 // this get route displays the name in the route that is greeted and displays how many times that name has been greeted
 
 app.get('/display/:name', async function (req, res, next) {
-    try {
-        let myName = req.params.name;
-        let nameCounter = await funcGreeting.oneName(myName);
-        res.render('countDisplay', { name: nameCounter.name, counter: nameCounter.counter });
-    }
-    catch (error) {
-        next(error.stack)
-    }
+  try {
+    let myName = req.params.name
+    let nameCounter = await funcGreeting.oneName(myName)
+    res.render('countDisplay', { name: nameCounter.name, counter: nameCounter.counter })
+  }
+  catch (error) {
+    next(error.stack)
+  }
 })
-
-
-
 let PORT = process.env.PORT || 3009
 
 app.listen(PORT, function () {
-    console.log('App starting on port', PORT)
+  console.log('App starting on port', PORT)
 
 })
